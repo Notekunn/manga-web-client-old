be4658e492b79480bd3fcccbd1f59755856dc03b
Cuong Tran <clonebmn2itt@gmail.com>
2021-02-04 00:38:04 +0700

    clean

--- src/features/app/alertSlice.js
+++ src/features/app/alertSlice.js
@@ -1,11 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-export const addNotification = createAsyncThunk('alert/create', async (noti, thunk) => {
-    await sleep(noti?.duration || 5);
-    return noti;
-})
+import { createSlice } from '@reduxjs/toolkit';
+
 const alertSlice = createSlice({
     name: "alert",
     initialState: {
@@ -33,26 +27,10 @@ const alertSlice = createSlice({
                 notices
             };
         }
-    },
-    extraReducers: {
-        [addNotification.pending]: (state, action) => {
-            const { type = "info", message, duration, key = "introduction" } = action?.meta?.arg || {};
-            const removeOldNotices = state.notices
-                .filter(e => e && e.key !== key);
-            removeOldNotices.push({ type, message, duration, key });
-            return {
-                notices: removeOldNotices
-            }
-        },
-        [addNotification.fulfilled]: (state, action) => {
-            const { key = "introduction" } = action?.meta?.arg || {};
-            return {
-                notices: state.notices.filter(e => e && e.key !== "introduction" && e.key !== key)
-            }
-        }
-    },
+    }
 });
 export const selectNotices = state => state.alert.notices;
 
 export const { showMessage, clearMessage } = alertSlice.actions;
+
 export default alertSlice.reducer;
