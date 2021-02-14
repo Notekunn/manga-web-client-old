7e55815ae5741a32867a1a91291dc3fb8a1128a5
Cuong Tran <clonebmn2itt@gmail.com>
2021-02-14 15:40:48 +0700

    fix smt

--- src/components/SideNavigation/index.js
+++ src/components/SideNavigation/index.js
@@ -8,13 +8,12 @@ function SideNavigation(props) {
   const history = useHistory();
   const onClick = useCallback(
     (e) => {
-      console.log(e);
       switch (e.key) {
         case 'goback':
           history.goBack();
           break;
         default:
-          history.push(`/${e.key}`);
+          history.push(`/${e.keyPath?.reverse().join('/')}`);
           break;
       }
     },
@@ -34,22 +33,22 @@ function SideNavigation(props) {
           Quản lý tài khoản
         </Menu.Item>
         <SubMenu key="manager" icon={<MailOutlined />} title="Quản lý truyện">
-          <Menu.Item key="manager/categories">Quản lý thể loại truyện</Menu.Item>
-          <Menu.Item key="manager/artists">Quản lý tác giả</Menu.Item>
-          <Menu.Item key="manager/translator-groups">Quản lý nhóm dịch</Menu.Item>
-          <Menu.Item key="manager/mangas">Quản lý truyện tranh</Menu.Item>
-          <Menu.Item key="manager/error">Báo lỗi</Menu.Item>
+          <Menu.Item key="categories">Quản lý thể loại truyện</Menu.Item>
+          <Menu.Item key="artists">Quản lý tác giả</Menu.Item>
+          <Menu.Item key="translator-groups">Quản lý nhóm dịch</Menu.Item>
+          <Menu.Item key="mangas">Quản lý truyện tranh</Menu.Item>
+          <Menu.Item key="error">Báo lỗi</Menu.Item>
         </SubMenu>
         <SubMenu key="request" icon={<MailOutlined />} title="Xét duyệt yêu cầu">
-          <Menu.Item key="request/create-translator-groups">Yêu cầu tạo nhóm dịch</Menu.Item>
-          <Menu.Item key="request/join-translator-groups">Yêu cầu tham gia nhóm dịch</Menu.Item>
-          <Menu.Item key="request/create-manga">Yêu cầu dịch truyện</Menu.Item>
-          <Menu.Item key="request/create-kind">Yêu cầu tạo tác giả, thể loại</Menu.Item>
+          <Menu.Item key="create-translator-groups">Yêu cầu tạo nhóm dịch</Menu.Item>
+          <Menu.Item key="join-translator-groups">Yêu cầu tham gia nhóm dịch</Menu.Item>
+          <Menu.Item key="create-manga">Yêu cầu dịch truyện</Menu.Item>
+          <Menu.Item key="create-kind">Yêu cầu tạo tác giả, thể loại</Menu.Item>
         </SubMenu>
         <SubMenu key="me" icon={<MailOutlined />} title="Yêu cầu của tôi">
-          <Menu.Item key="me/join-translator-groups">Yêu cầu tham gia nhóm dịch</Menu.Item>
-          <Menu.Item key="me/create-manga">Yêu cầu dịch truyện</Menu.Item>
-          <Menu.Item key="me/create-kind">Yêu cầu tạo tác giả, thể loại</Menu.Item>
+          <Menu.Item key="join-translator-groups">Yêu cầu tham gia nhóm dịch</Menu.Item>
+          <Menu.Item key="create-manga">Yêu cầu dịch truyện</Menu.Item>
+          <Menu.Item key="create-kind">Yêu cầu tạo tác giả, thể loại</Menu.Item>
         </SubMenu>
         <Menu.Item key="goback" icon={<ContainerOutlined />}>
           Go Back
